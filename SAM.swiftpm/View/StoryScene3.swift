58ce3e60b0836095d30f9fc38e54e7248953d1a0
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-03-28 17:55:57 +0900

    Final commit for basic frame

--- SAM.swiftpm/View/StoryScene3.swift
+++ SAM.swiftpm/View/StoryScene3.swift
@@ -9,6 +9,6 @@ import SwiftUI
 
 struct StoryScene3: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, StoryScene3!")
     }
 }

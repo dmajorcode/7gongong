5792ee02b6cc4d831d5f120bc30bac88310b561e
dmajorcode <dianekang93@gmail.com>
2023-03-29 16:18:30 +0900

    fix: velocity problem

--- SAM.swiftpm/ContentView.swift
+++ SAM.swiftpm/ContentView.swift
@@ -21,22 +21,22 @@ struct ContentView: View {
                     Image(systemName: "1.circle")
                 })
             
-            StoryScene()
+            StoryScene1()
                 .tabItem({
                     Image(systemName: "2.circle")
                 })
             
-            StoryScene()
+            StoryScene2()
                 .tabItem({
                     Image(systemName: "3.circle")
                 })
             
-            StoryScene()
+            StoryScene3()
                 .tabItem({
                     Image(systemName: "4.circle")
                 })
             
-            StoryScene()
+            StoryScene4()
                 .tabItem({
                     Image(systemName: "5.circle")
                 })

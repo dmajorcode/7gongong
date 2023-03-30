c4d171579e3171730f3ecdbea973c94e25417259
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-03-30 12:44:35 +0900

    Feat: "Add PageCover"

---
+++ SAM.swiftpm/View/BookcoverImage.swift
@@ -0,0 +1,32 @@
+//
+//  BookcoverImage.swift
+//  SAM
+//
+//  Created by 077tech on 2023/03/30.
+//
+
+import SwiftUI
+
+struct BookcoverImage: View {
+    
+    @State private var showingDetail = false
+    
+    var body: some View {
+        Image("Stage_1")
+            .resizable()
+            .renderingMode(.original)
+            .ignoresSafeArea()
+            .onTapGesture {
+                showingDetail.toggle()
+        }
+            .fullScreenCover(isPresented: $showingDetail) {
+                ContentView()
+            }
+    }
+}
+
+struct BookcoverImage_Previews: PreviewProvider {
+    static var previews: some View {
+        BookcoverImage()
+    }
+}

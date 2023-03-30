a0fba2ce303484f1743feeebf21237f3881b6124
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-03-30 11:52:43 +0900

    Feat: "스스겜 완료" + "영,지쿠 이미지 추가"

--- SAM.swiftpm/View/OnBoardingPage.swift
+++ SAM.swiftpm/View/OnBoardingPage.swift
@@ -9,37 +9,102 @@ import SwiftUI
 import SpriteKit
 
 struct OnBoardingPage: View {
+    
+    @State var nextButton = false
+    @State var storyImage : String = "daaanImage1"
+    @State var storyImage2 : String = "maltyChadStory"
+    @State var imageToggle : Bool = true
+    @State var goToGame : Int = 0
+    
     var scene = GameScene1()
     @State private var useSpriteView = false
-
-var body: some View {
-    ZStack{
-            if useSpriteView {
-                SpriteView(scene: scene).ignoresSafeArea()
-            } else {
-                Image("jikuStory")
+    
+    var body: some View {
+        ZStack{
+            if imageToggle{
+                Image("\(storyImage)")
                     .resizable()
-                    .frame(width: 400, height: 775)
+                    .renderingMode(.original)
                     .ignoresSafeArea()
-                HStack{
-                    Spacer()
-                    VStack{
-                        Spacer()
+            }else{
+                Image("\(storyImage2)")
+                    .resizable()
+                    .renderingMode(.original)
+                    .ignoresSafeArea()
+            }
+
+            HStack {
+                Spacer()
+                ZStack{
+                    if goToGame == 0{
+                        //
                         Button(action: {
-                            self.useSpriteView.toggle()
+                            nextButton = true
+                            imageToggle = false
+                            if imageToggle == false{
+                                goToGame = 1
+                            }
                         }) {
                             Image(systemName: "arrowshape.right.fill")
                                 .resizable()
                                 .frame(width: 30, height: 40)
                         }
-                        Spacer()
+                        .padding(10)
+                        .foregroundColor(.black)
+                        //
+                    }else{
+                        if useSpriteView {
+                            SpriteView(scene: scene)
+                                .frame(width: 440, height: nil)
+                                .ignoresSafeArea()
+                                
+                        }else{
+                            Button(action: {
+                                self.useSpriteView.toggle()
+                            }) {
+                                Image(systemName: "arrowshape.right.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 40)
+                            }
+                            .padding(10)
+                            .foregroundColor(.black)
+                        }
                     }
-                    .padding(10)
-                    .foregroundColor(.black)
                 }
             }
         }
     }
+//    var scene = GameScene1()
+//    @State private var useSpriteView = false
+//
+//var body: some View {
+//    ZStack{
+//            if useSpriteView {
+//                SpriteView(scene: scene).ignoresSafeArea()
+//            } else {
+//                Image("jikuStory")
+//                    .resizable()
+//                    .frame(width: 400, height: 775)
+//                    .ignoresSafeArea()
+//                HStack{
+//                    Spacer()
+//                    VStack{
+//                        Spacer()
+//                        Button(action: {
+//                            self.useSpriteView.toggle()
+//                        }) {
+//                            Image(systemName: "arrowshape.right.fill")
+//                                .resizable()
+//                                .frame(width: 30, height: 40)
+//                        }
+//                        Spacer()
+//                    }
+//                    .padding(10)
+//                    .foregroundColor(.black)
+//                }
+//            }
+//        }
+//    }
 
 }
 
